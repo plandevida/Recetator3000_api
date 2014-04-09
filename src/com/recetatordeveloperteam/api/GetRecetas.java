e23f637954b850d3798498ffe4697e72f362b989
emilalvap <emil.alvap@gmail.com>
2014-04-09 20:46:59 +0200

    Incluido el Login servlet

---
+++ src/com/recetatordeveloperteam/api/GetRecetas.java
@@ -0,0 +1,21 @@
+package com.recetatordeveloperteam.api;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class GetRecetas extends HttpServlet { 
+	
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		String nombre = req.getParameter("nombre");
+		resp.getWriter().println("nombre:" +nombre);
+		
+		
+		
+	}
+}
