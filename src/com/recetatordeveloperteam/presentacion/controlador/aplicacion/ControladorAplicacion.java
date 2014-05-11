a202227d73241d9315e8c34e16fb1954134db48c
Juan L. Pérez <jlpv_90@hotmail.com>
2014-05-11 17:16:14 +0200

    One entry point to the app

--- src/com/recetatordeveloperteam/presentacion/controlador/aplicacion/ControladorAplicacion.java
+++ src/com/recetatordeveloperteam/presentacion/controlador/aplicacion/ControladorAplicacion.java
@@ -1,9 +1,17 @@
 package com.recetatordeveloperteam.presentacion.controlador.aplicacion;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import com.recetatordeveloperteam.presentacion.comandos.IDEventos;
 import com.recetatordeveloperteam.presentacion.controlador.aplicacion.imp.ControladorAplicacionImp;
 
-public abstract class ControladorAplicacion {
+public abstract class ControladorAplicacion extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -9048064705085705849L;
 	private static ControladorAplicacion controladorAplicacionInstance;
 	
 	public static ControladorAplicacion getInstance() {
@@ -16,5 +24,5 @@ public abstract class ControladorAplicacion {
 			controladorAplicacionInstance = new ControladorAplicacionImp();
 	}
 	
-	public abstract void handleRequest(IDEventos evento, Object datos);
+	
 }
--- src/com/recetatordeveloperteam/presentacion/controlador/aplicacion/imp/ControladorAplicacionImp.java
+++ src/com/recetatordeveloperteam/presentacion/controlador/aplicacion/imp/ControladorAplicacionImp.java
@@ -1,14 +1,45 @@
 package com.recetatordeveloperteam.presentacion.controlador.aplicacion.imp;
 
-import com.recetatordeveloperteam.presentacion.comandos.IDEventos;
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import com.recetatordeveloperteam.presentacion.controlador.aplicacion.ControladorAplicacion;
 
-public class ControladorAplicacionImp extends ControladorAplicacion {
+public class ControladorAplicacionImp extends ControladorAplicacion   {
 
-	@Override
-	public void handleRequest(IDEventos evento, Object datos) {
-		// TODO Auto-generated method stub
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5688085232129774014L;
+	protected void service(HttpServletRequest req,
+            HttpServletResponse resp)
+	{
+		resp.setContentType("text/plain");
+	try {
+		resp.getWriter().println("guatdafaq");
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
 		
 	}
-
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) 
+	{
+		
+	}
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
+	{
+		
+	}
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) 
+	{
+		
+	}
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) 
+	{
+		
+	}
 }
